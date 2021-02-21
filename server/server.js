e993e02638328cce752bb8f18403f5e7331c04f2
Yashua Pervez <yashuapervez@gmail.com>
2021-02-21 23:33:06 +0500

    Add and Read todo route setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/Todo");
 const {User} = require("./models/User");
 
 const express = require("express");
+const {ObjectID} = require("mongodb");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -20,18 +22,38 @@ app.post('/todos', (req, res) => {
     });
 });
 
-// const newUser = new User({
-//     email: "         yashuapervez@gmail.com    ",
-//     age: 10,
-//     password: "elisha123"
-// });
+app.get("/todos", (req, res) => {
+    //res.send("Received");
+    Todo.find()
+    .then(todos => {
+        res.send({data: todos})
+    }, e => {
+        res.status(400).send("Error Occured: ", e);
+    });
+});
 
-// newUser.save()
-// .then(doc => {
-//     console.log("Saved Todo", doc);
-// }, err => {
-//     console.log("Unable to save todo");
-// });
+app.get("/todos/:id", (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send({
+            error: "Todo not found",
+        });
+    }
+    Todo.findById(id)
+    .then(doc => {
+        if(doc){
+            res.status(200).send(doc);
+        }
+        
+        res.status(404).send({
+            error: "Todo not found",
+        })
+    }, error => {
+        res.status(400).send({
+            error: "Something bad happened",
+        })
+    })
+});
 
 app.listen(3000, () => {
     console.log("App Running At Port 3000");
